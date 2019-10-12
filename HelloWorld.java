50d28b1acc05659e881a238f66a5c17110d8485c
IStar_Lee <IStar_Lee@126.com>
2019-10-12 17:56:32 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public void sayHello(){
+		System.out.println("Hello World1");
+		System.out.println("Hello World2");
+		System.out.println("Hello World3");
+		System.out.println("Hello World4");
+	}
+}
